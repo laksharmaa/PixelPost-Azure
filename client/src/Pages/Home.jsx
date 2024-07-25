c9df3527935888d304b5e0e6a93f009e55e7ce76
Lakshya Sharma <lakshya.00ls05@gmail.com>
2024-07-25 20:20:10 +0530

    updated Create-Post page and FormField

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Loader, Card, FormField} from '../components';
 const Home = () => {
   const [loading, setLoading] = useState(false);
