116af5172a71f8f8ed48cb450aabe7626913c632
Lakshya Sharma <lakshya.00ls05@gmail.com>
2024-07-31 02:25:49 +0530

    created mogodb model, dalleRoutes, postRoutes, connected openai api, updated setGeneratingImg logic in CreatePost

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -15,7 +15,28 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => { }
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        })
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.image}` })
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert("Please enter a prompt")
+    }
+  }
 
   const handleSubmit = () => {
     // Add your form submission logic here
@@ -89,18 +110,18 @@ const CreatePost = () => {
             type='button'
             onClick={generateImage}
             className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
-            >
-              {generatingImg? 'Generating Image...' : 'Generate Image'}
-            </button>
+          >
+            {generatingImg ? 'Generating Image...' : 'Generate Image'}
+          </button>
         </div>
 
         <div className='mt-10'>
           <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in community</p>
           <button
-          type='submit'
-          className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2,5 text-center'
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2,5 text-center'
           >
-            {loading? "Sharing...": "Share with the community"}
+            {loading ? "Sharing..." : "Share with the community"}
 
           </button>
         </div>
