6b91d1f93a59cc8c138281316b673842bcd2cf66
Lakshya Sharma <lakshya.00ls05@gmail.com>
2024-09-12 15:36:18 +0530

    updated cors policy

--- server/index.js
+++ server/index.js
@@ -8,8 +8,15 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({limit: '50mb'}));
+
+const corsOptions = {
+    origin: 'https://pixel-post.vercel.app', // or use '*' to allow all origins (not recommended for production)
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+// app.use(cors());
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -17,12 +17,6 @@ router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
 
-        // const aiResponse = await openai.createImage({
-        //     prompt,
-        //     n:1,
-        //     size: "1024x1024",
-        //     response_format: "b64_json",
-        // });
         const aiResponse = await openai.images.generate({
             prompt: prompt,
             n: 1,
